9856b0f72563429dc26052a8dcce451adb4b383a
Shevyakov Ilya <ilya.shevyakov00@mail.ru>
2020-03-16 21:19:30 +0300

    Added a program

---
+++ DEV-1/DEV-1/line.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_1
+{
+    public  class Line
+    {
+       
+        private string _line;
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public Line (string Word)
+        {
+           word = Word;
+        }
+        /// <summary>
+        /// Method that set and return field values
+        /// </summary>
+        public string word
+        {
+            get
+            {
+                return _line;
+            }
+            set
+            {
+                this._line = value ;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool IsLineEmpty()
+        {
+            if (word == "")
+            {
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Throw exception if value of string is null
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public string  checkValue(string value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentException();
+            }
+            return value;
+        }
+        /// <summary>
+        /// Method counts number of non repeating symbols in line
+        /// </summary>
+        /// <returns></returns>
+        public int NonRepeatingSymbols()
+        {
+            int count = 0;
+            int max = 0;
+            char buffer = '\0';
+            buffer = word[0];
+            for (int i = 1; i < word.Length; ++i)
+            {
+                if (word[i] != buffer)
+                {
+                    count++;
+                }
+                else
+                {
+                    if (max < count)
+                    {
+                        max = count;
+                    }
+                    count = 1;
+                    buffer = word[0];
+                }
+            }
+            if (count > max)
+            {
+                max = count;
+
+                return max;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        
+    }
+}
---
+++ DEV-1/UnitTest1/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using  DEV_1;
+using System;
+
+namespace DEV_1.Test
+{
+    [TestClass()]
+    public class LineTest
+    {
+        [TestMethod()]
+        public void IsLineEmptyTure()
+        {
+            DEV_1.Line word = new DEV_1.Line("");
+
+            bool actual = word.IsLineEmpty();
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void IsLineEmptyFalse()
+        {
+            Line word = new Line("abcd");
+
+            bool actual = word.IsLineEmpty();
+
+            Assert.IsFalse(actual);
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CheckValueNullTest()
+        {
+            Line symbols = new Line(null);
+        }
+        [TestMethod()]
+        public void EmptyLineNumberOfNonRepeatingSymbolsTest()
+        {
+            Line word = new Line("");
+            int expected = 0;
+
+            int actual = word.NonRepeatingSymbols();
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void NumberOfNonRepeatingSymbolsTest()
+        {
+            Line word = new Line("abccefgh");
+            int expected = 5;
+
+            int actual = word.NonRepeatingSymbols();
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void SameSymblosNumberOfNonRepeatingSymbolsTest()
+        {
+            Line word = new Line("aaaaa");
+            int expected = 1;
+
+            int actual = word.NonRepeatingSymbols();
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
