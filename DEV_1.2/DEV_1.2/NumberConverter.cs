3586216b30de6904d44c9d75de717bfc55e6a20f
Shevyakov Ilya <ilya.shevyakov00@mail.ru>
2020-03-18 16:51:03 +0300

    Finished DEV_1. 2, added tests and comments

---
+++ DEV_1.2/DEV_1.2/NumberConverter.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace DEV_1._2
+{
+   public  class NumberConverter
+    {
+        private int _number;
+        private int _systemNumber;
+        /// <summary>
+        /// constructor 
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="systemNumber"></param>
+        public NumberConverter( int number, int  systemNumber)
+        {
+            Number = number;
+            SystemNumber = systemNumber;
+        }
+        /// <summary>
+        /// check the type of   number
+        /// </summary>
+        /// <param name="value"></param>
+        public void CheckNumber(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentNullException();
+            }
+           
+        }
+        /// <summary>
+        /// Number accepts and returns values
+        /// </summary>
+        public int Number
+        {
+           
+            set
+            {
+                CheckNumber(value);
+                _number = value;
+            }
+            get
+            {
+                return _number;
+            }
+        }
+        /// <summary>
+        /// checks the order of the number system
+        /// </summary>
+        /// <param name="value"></param>
+        public void CheckSystemNumber(int value)
+        {
+            if (value <2)
+            {
+                throw new ArgumentException();
+            }
+            if (value >20)
+            {
+                throw new Exception();
+            }
+
+        }
+        /// <summary>
+        /// System accepts and returns values
+        /// </summary>
+        public int SystemNumber
+        {
+            set
+            {
+                CheckSystemNumber(value);
+                _systemNumber = value;
+            }
+            get
+            {
+                return _systemNumber;
+            }
+        }
+        /// <summary>
+        /// translates a number from one number system to another
+        /// </summary>
+        /// <returns></returns>
+        public string TransferToNewSystem()
+        {
+            int remains = 0;
+            List<string> TheOldNumber = new List<string>();
+
+            while (Number > 0)
+            {
+                remains = Number % SystemNumber;
+                Number = Number / SystemNumber;
+                CheckRemains(remains, TheOldNumber);
+            }
+
+            return CorrectNumber(TheOldNumber);
+        }
+        /// <summary>
+        /// returns the List as a string in reverse order
+        /// </summary>
+        /// <param name="TheOldNumber"></param>
+        /// <returns></returns>
+        public static string CorrectNumber(List<string> TheOldNumber)
+        {
+            List<string> Numbers = new List<string>();
+
+            for (int i = TheOldNumber.Count - 1; i >= 0; i--)
+            {
+                Numbers.Add(TheOldNumber[i]);
+            }
+
+            return string.Join(String.Empty, Numbers);
+        }
+        /// <summary>
+        /// checks the remainder of the division
+        /// </summary>
+        /// <param name="remains"></param>
+        /// <param name="TheOldNumber"></param>
+        /// <returns></returns>
+        public List<string> CheckRemains(int remains, List<string> TheOldNumber)
+        {
+            if (remains > 9)
+            {
+                TheOldNumber.Add(DivisorGreaterThanNine(remains.ToString()));
+                return TheOldNumber;
+            }
+
+            else
+            {
+                TheOldNumber.Add(remains.ToString());
+                return TheOldNumber;
+            }
+        }
+        /// <summary>
+        /// returns the letter to the corresponding remainder
+        /// </summary>
+        /// <param name="remains"></param>
+        /// <returns></returns>
+        public static  string DivisorGreaterThanNine(string remains)
+        {
+            switch (remains)
+            {
+                case "10":
+                return "A";
+                case "11":
+                return "B";
+                case "12":
+                return "C";
+                case "13":
+                return "D";
+                case "14":
+                return "E";
+                case "15":
+                return "F";
+                case "16":
+                return "G";
+                case "17":
+                return "H";
+                case "18":
+                return "I";
+                case "19":
+                return "J";
+            }
+
+            return String.Empty;
+        }
+    }
+}
---
+++ DEV_1.2/NumberConverterTests/UnitTest1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using DEV_1._2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace NumberConverterTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("10", "A")]
+        [DataRow("11", "B")]
+        [DataRow("12", "C")]
+        [DataRow("13", "D")]
+        [DataRow("14", "E")]
+        [DataRow("15", "F")]
+        [DataRow("16", "G")]
+        [DataRow("17", "H")]
+        [DataRow("18", "I")]
+        [DataRow("19", "J")]
+        public void DivisorGreaterThanNineTest(string number, string expected)
+        {
+            Assert.AreEqual(expected, NumberConverter.DivisorGreaterThanNine(number));
+        }
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(10, 2, "1010")]
+        [DataRow(10, 3, "101")]
+        [DataRow(10, 4, "22")]
+        [DataRow(10, 5, "20")]
+        [DataRow(10, 6, "14")]
+        [DataRow(10, 7, "13")]
+        [DataRow(10, 8, "12")]
+        [DataRow(10, 9, "11")]
+        [DataRow(10, 10, "10")]
+        [DataRow(10, 11, "A")] 
+        [DataRow(10, 12, "A")]
+        [DataRow(11, 13, "B")]
+        [DataRow(12, 14, "C")]
+        [DataRow(13, 15, "D")]
+        [DataRow(14, 16, "E")]
+        [DataRow(15, 17, "F")]
+        [DataRow(16, 18, "G")]
+        [DataRow(17, 19, "H")]
+        [DataRow(18, 20, "I")]
+        public void TransferToNewSystem(int number, int system, string expected)
+        {
+            NumberConverter systemNumber =
+                new NumberConverter(number, system);
+            Assert.AreEqual(expected, systemNumber.TransferToNewSystem());
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CheckLessThenTwo()
+        {
+            NumberConverter systemNumber = new NumberConverter(10, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void CheckMoreThenTwenty()
+        {
+            NumberConverter systemNumber = new NumberConverter(10, 22);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NumberLessThenZero()
+        {
+            NumberConverter systemNumber = new NumberConverter(-10, 2);
+        }
+
+        
+    }
+}
