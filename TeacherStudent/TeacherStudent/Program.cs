beebadcbd0cd4716656a11dab53bf2b9f2436e65
Shevyakov Ilya <ilya.shevyakov00@mail.ru>
2020-03-27 12:12:12 +0300

    TeacherStudent

---
+++ TeacherStudent/TeacherStudent/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher();
+
+            Student[] students = new Student[10];
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i] = new Student($"FirstName{i + 1}", $"LastName{i + 1}");
+                teacher.StudentDone(students[i]);
+            }
+
+            foreach (Student student in students)
+            {
+                student.DoTask();
+            }
+        }
+    }
+}
---
+++ TeacherStudent/TeacherStudent/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherStudent
+{
+    class Student : ITaskPerfomer
+    {
+        private string _name;
+        private string _surname;
+
+        public delegate void StudentTask(Student student, string answer);
+        public event StudentTask HomeworkDone;
+        public Student(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+
+            set
+            {
+                _surname = value;
+            }
+        }
+
+        public static Random random = new Random((int)DateTime.Now.Ticks);
+        
+        public void DoTask()
+        {
+            int answerLength = random.Next(1, 10);
+            string answer = String.Empty;
+            for (int i = 0; i < answerLength; i++)
+            {
+                answer += $"{(char)random.Next(65, 90)}";
+            }
+            HomeworkDone?.Invoke(this, answer);
+        }
+
+        string ITaskPerfomer.DoTask()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
